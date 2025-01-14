0d5cbd5f0624b34e5e5854b98da35d52c9b3b8b6
Andrew <andrewunifei@gmail.com>
2025-01-14 00:13:27 -0300

    [ADD] Continuation of content creation

--- src/app/cleanCode/components/CleanCodeHeader.tsx
+++ src/app/cleanCode/components/CleanCodeHeader.tsx
@@ -1,5 +1,12 @@
+"use client"
+
+import * as React from 'react';
 import { GitHub, LinkedIn, X } from '@mui/icons-material';
-import { Box } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, Link } from "@mui/material"
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 
 const externaLinks = {
     'github': "http://www.github.com/andrewunifei",
@@ -8,13 +15,45 @@ const externaLinks = {
 }
 
 export default function CleanCodeHeader() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <Box className="flex justify-between items-center py-10">
             <h1 className="text-3xl font-helvetica">Resumo Clean Code</h1>
-            <Box  className="flex justify-between items-center w-24">
+            <Box  className="flex justify-between items-center w-32">
                 <a href={externaLinks['github']} target="_blank"><GitHub /></a>
                 <a href={externaLinks['linkedin']} target="_blank"><LinkedIn /></a>
-                <a href={externaLinks['x']} target="_blank"><X /></a>                    
+                <a href={externaLinks['x']} target="_blank"><X /></a>  
+                <div>
+                    <a
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        className='cursor-pointer'
+                    >
+                        <MenuIcon /> 
+                    </a>       
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>Andrew's site</MenuItem>
+                        <MenuItem onClick={handleClose}>Trading</MenuItem>
+                    </Menu> 
+                </div>   
             </Box>
         </Box>
     )
--- src/app/cleanCode/page.tsx
+++ src/app/cleanCode/page.tsx
@@ -14,7 +14,7 @@ export default function CleanCode() {
             <div className="bg-[#000] px-8">
                 <BlackStrip />
             </div>
-            <div className="bg-[#F5F5F5] px-8 pb-64 pt-8">        
+            <div className="bg-[#F5F5F5] px-8 pb-64 pt-10">        
                 <Container maxWidth="xl" className="bg-[#F5F5F5] h-full">   
                     <CleanCodeSection />
                     <CleanCodeSection />
