386a1fd1765160a6f1cb0382977b86889e52fa21
Andrew <andrewunifei@gmail.com>
2025-01-13 22:41:59 -0300

    [ADD] Continuation of content creation

--- src/app/cleanCode/page.tsx
+++ src/app/cleanCode/page.tsx
@@ -4,9 +4,10 @@ import { Container, Box } from "@mui/material"
 
 export default function CleanCode() {
     return (
-        <div className="bg-[#F5F5F5] pb-32 px-8 h-full">
-            <Container maxWidth="xl" className="bg-[#F5F5F5]">    
+        <div className="bg-[#F5F5F5] px-8 pb-64">
+            <Container maxWidth="xl" className="bg-[#F5F5F5] h-full">    
                 <CleanCodeHeader />
+                <CleanCodeSection />
                 <CleanCodeSection />
             </Container>  
         </div>       
