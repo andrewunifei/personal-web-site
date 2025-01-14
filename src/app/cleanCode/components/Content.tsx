e4a2662120becc9a927cff66007775ae7853e171
Andrew <andrewunifei@gmail.com>
2025-01-13 23:15:29 -0300

    [ADD] Continuation of content creation

--- src/app/cleanCode/components/Content.tsx
+++ src/app/cleanCode/components/Content.tsx
@@ -3,35 +3,38 @@ import { Paper, Box, Divider } from "@mui/material"
 export default function Content() {
     return (
         <>
-            <Paper elevation={0} square={true} className="basis-1/2 p-8 h-full">
-                <Box className="flex justify-between items-center pb-3">
-                    <p className="text-stone-500 text-sm"><span className="italic">Clean Code</span> por Robert Cecil Martin</p>
-                    <p className="font-extrabold">00</p>
+            <Paper elevation={0} square={true} className="basis-1/2 p-8 flex flex-col">
+                <Box className="flex flex-col w-full">
+                    <Box className="flex justify-between items-center pb-3">
+                        <p className="text-stone-500 text-sm"><span className="italic">Clean Code</span> por Robert Cecil Martin (Resumo)</p>
+                        <p className="font-extrabold">00</p>
+                    </Box>
+                    <h1 className="text-3xl font-extrabold">Introdução</h1>
                 </Box>
-                <h1 className="text-3xl font-extrabold pb-24">Introdução</h1>
-                <br />
-                <p>
-                    Você deve cultivar conhecimentos sobre Princípios, Padrões, Práticas, e Heurísticas que um artesão de código possui, e você também precisa colocar em prática esses conhecimentos. Trabalho duro, não basta apenas saber.
-                </p>
-                <br />
-                <p>
-                    O livro é dividido em três partes: 
-                </p>
-                <br />
-                <p>
-                    1. Descrição de princípios, padrões, e práticas para escrever códigos limpos. Vários exemplos em código nessa parte.
-                </p>
-                <p>
-                    2. Vários estudos de casos na parte 2, cada vez mais complexos. Cada estudo de caso consiste em um exercício de refatoração de código. De acordo com o autor, é a parte mais densa e intricada do livro, exige foco e paciência do leitor, e também revisão de páginas anteriores.
-                </p>
+                <Box className="flex flex-col justify-center h-full">
+                    <p>
+                        Você deve cultivar conhecimentos sobre Princípios, Padrões, Práticas, e Heurísticas que um artesão de código possui, e você também precisa colocar em prática esses conhecimentos. Trabalho duro, não basta apenas saber.
+                    </p>
+                    <br />
+                    <p>
+                        O livro é dividido em três partes: 
+                    </p>
+                    <br />
+                    <p>
+                        <span className="font-bold">1.</span> Descrição de princípios, padrões, e práticas para escrever códigos limpos. Vários exemplos em código nessa parte.
+                    </p>
+                    <p>
+                        <span className="font-bold">2.</span> Vários estudos de casos na parte 2, cada vez mais complexos. Cada estudo de caso consiste em um exercício de refatoração de código. De acordo com o autor, é a parte mais densa e intricada do livro, exige foco e paciência do leitor, e também revisão de páginas anteriores.
+                    </p>
 
-                <p>
-                    3. A recompensa para o leitor está na parte 3. Um único capítulo que contém uma lista de heurísticas e abordagens para refatorar códigos. Todos os motivos para mudanças e o processo de pensamentos e critérios são documentados aqui.
-                </p>
+                    <p>
+                        <span className="font-bold">3.</span> A recompensa para o leitor está na parte 3. Um único capítulo que contém uma lista de heurísticas e abordagens para refatorar códigos. Todos os motivos para mudanças e o processo de pensamentos e critérios são documentados aqui.
+                    </p>
+                </Box>
             </Paper>
             <Divider orientation="vertical" className="bg-[#B5B5B5]" flexItem/>
-            <Paper elevation={0} square={true} className="basis-1/2 p-8 w-full flex flex-col bg-[#000]">
-                <Box className="flex justify-center w-full items-center">
+            <Paper elevation={0} square={true} className="basis-1/2 p-8 flex flex-col">
+                <Box className="flex justify-center w-full items-center pb-3">
                     <p className="font-extrabold text-sm">PT. 0</p>
                 </Box>
                 <Box className="flex flex-col justify-center h-full">
